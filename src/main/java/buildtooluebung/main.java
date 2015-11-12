d877940b92410836c4ebcad2a815b0a1dbf31114
Standard <Standard>
2015-11-12 18:03:06 +0100

    integration and unit test

--- src/main/java/buildtooluebung/main.java
+++
@@ -1,8 +0,0 @@
-package buildtooluebung;
-
-public class main {
-
-	public static void main(String args[]){
-		System.out.println("Hello Gradle");
-	}
-}
---
+++ src/main/java/fhb/App.java
@@ -0,0 +1,22 @@
+package fhb;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println(getHelloWorld());
+
+	}
+
+	public static String getHelloWorld() {
+
+		return "Hello World";
+
+	}
+
+	public static String getHelloWorld2() {
+
+		return "Hello World 2";
+
+	}
+
+}
